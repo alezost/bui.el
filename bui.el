30b8f9eea235c285e634da1b0e8d7cb9d44e65f1
Alex Kost <alezost@gmail.com>
2016-10-20 16:55:46 +0300

    Add README and improve package description

--- bui.el
+++ bui.el
@@ -1,4 +1,4 @@
-;;; bui.el --- Buffer interface for displaying data  -*- lexical-binding: t -*-
+;;; bui.el --- Buffer interface library  -*- lexical-binding: t -*-
 
 ;; Copyright © 2014-2016 Alex Kost <alezost@gmail.com>
 
@@ -23,8 +23,20 @@
 
 ;;; Commentary:
 
-;; This file provides a general 'buffer' interface for displaying an
-;; arbitrary data.
+;; BUI (Buffer User Interface) is a library for making 'list' (similar
+;; to "M-x list-packages") and 'info' (similar to customization buffers)
+;; interfaces to display various data (packages, buffers, functions,
+;; etc.).
+;;
+;; It is not an end-user package, it is a library that is intended to be
+;; used by other packages.
+;;
+;; Basically, at first you define 'list'/'info' interface using
+;; `bui-define-interface' macro, and then you can make user commands
+;; that will display entries using `bui-get-display-entries' and similar
+;; functions.
+;;
+;; See README at <https://github.com/alezost/bui.el> for more details.
 
 ;;; Code:
 
